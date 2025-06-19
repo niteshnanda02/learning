c393fe96625f6f0862c9aa53c18557ac042e67f9
niteshnanda02 <niteshnanda02@gmail.com>
2025-06-19 15:29:05 +0530

    add leak bucket

--- src/main/java/org/example/atlassian/ratelimiter/enums/RateLimiterEnum.java
+++ src/main/java/org/example/atlassian/ratelimiter/enums/RateLimiterEnum.java
@@ -1,5 +1,5 @@
 package org.example.atlassian.ratelimiter.enums;
 
 public enum RateLimiterEnum {
-    TOKEN, SLIDING_WINDOW_LOG, FIXED, CREDIT_TOKEN_BUCKET
+    TOKEN, SLIDING_WINDOW_LOG, FIXED, CREDIT_TOKEN_BUCKET, LEAK
 }
